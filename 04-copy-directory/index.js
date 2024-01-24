85563e46e3d1158b5c3f5ee2eaf71cb7f8a4fa65
valeryaosta <gerasimenko.valery@gmail.com>
2024-01-24 23:24:04 +0400

    feat: add fn for clear directory for the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,9 +4,23 @@ const path = require('path');
 const srcDir = path.join(__dirname, 'files');
 const destDir = path.join(__dirname, 'files-copy');
 
+async function clearDirectory(directory) {
+  const files = await fs.readdir(directory, { withFileTypes: true });
+  for (const file of files) {
+    const filePath = path.join(directory, file.name);
+    if (file.isDirectory()) {
+      await clearDirectory(filePath);
+      await fs.rmdir(filePath);
+    } else {
+      await fs.unlink(filePath);
+    }
+  }
+}
+
 async function copyDir() {
   try {
     await fs.mkdir(destDir, { recursive: true });
+    await clearDirectory(destDir);
 
     const files = await fs.readdir(srcDir);
 
